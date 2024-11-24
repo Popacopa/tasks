6a50d36082deeedf59b3787315d1f99c7e90e9e4
Popacopa <paushakow@gmail.com>
2024-11-24 18:34:52 +0300

    clean up class List

--- linked_list.cpp
+++ linked_list.cpp
@@ -3,74 +3,94 @@
 
 class Node {
 public:
+    Node(int _data) : data(_data), next(nullptr) {}
     int data;
     Node* next;
-    Node(int _data) : data(_data), next(nullptr){}
-
 };
-struct List {
-    Node *begin, *end;
-    List() : begin(nullptr), end(nullptr){}
+
+class List {
+public:
+    List() : begin(nullptr), end(nullptr) {}
     ~List() {
-        while (!is_empty()) {this->pop();}
-    }
-bool is_empty() {
-    return begin == nullptr;
-}
-void print() {
-    if (is_empty()) {std::cout << "elements is not exist!";}
-    Node* ptr = begin;
-    while (ptr != nullptr) {
-        if (ptr->next == nullptr) {std::cout << ptr->data << "\n";} else {std::cout << ptr->data << " ";}
-        ptr = ptr->next;
-    }
-}
-int len() {
-    int lenght{};
-    Node* ptr = begin;
-    while (ptr != nullptr) {
-        lenght++;
-        ptr = ptr->next;
-    }
-    return lenght;
-}
-Node* get_node(int index) {
-    if (index > len() || index < 0) {return nullptr;}
-    Node* ptr = begin;
-    for (int i{}; i < len(); i++) {
-        if (i == index) {return ptr;}
-        ptr = ptr->next;
-    } 
-    return nullptr;  
-}
-Node* append(int _data) {
-    Node* obj = new Node(_data);
-    is_empty() ? (begin = end = obj) : (end->next = obj, end = obj);
-    return obj;
-}
-Node* insert(int _data) {
-    Node* obj = new Node(_data);
-    is_empty() ? (begin = end = obj) : (obj->next = begin, begin = obj);
-    return obj;
+        while (!is_empty()) {
+            pop();
+        }
     }
 
-void pop() {
-    Node* buffer = begin->next;
-    delete begin;
-    begin = buffer;
+    void print() const {
+        if (is_empty()) {
+            std::cout << "elements do not exist!\n";
+            return;
+        }
+        Node* ptr = begin;
+        while (ptr != nullptr) {
+            std::cout << ptr->data << (ptr->next ? " " : "\n");
+            ptr = ptr->next;
+        }
+    }
+
+    int len() const {
+        int length = 0;
+        Node* ptr = begin;
+        while (ptr != nullptr) {
+            ++length;
+            ptr = ptr->next;
+        }
+        return length;
+    }
+
+    Node* get_node(int index) const {
+        if (index < 0) {
+            return begin;
+        } else if (index > len()) {
+            return end;
+        }
+        Node* ptr = begin;
+        for (int i = 0; i < index; ++i) {
+            ptr = ptr->next;
+        }
+        return ptr;
+    }
+
+    Node* append(int _data) {
+        Node* obj = new Node(_data);
+        if (is_empty()) {
+            begin = end = obj;
+        } else {
+            end->next = obj;
+            end = obj;
+        }
+        return obj;
+    }
+
+    Node* insert(int _data) {
+        Node* obj = new Node(_data);
+        if (is_empty()) {
+            begin = end = obj;
+        } else {
+            obj->next = begin;
+            begin = obj;
+        }
+        return obj;
+    }
+
+    void pop() {
+        if (!is_empty()) {
+            Node* buffer = begin->next;
+            delete begin;
+            begin = buffer;
+            if (begin == nullptr) {
+                end = nullptr;
+            }
+        }
+    }
+
+private:
+    bool is_empty() const {
+        return begin == nullptr;
+    }
+
+    Node* begin;
+    Node* end;
 };
-};
-int main() {
-    List arr;
-    Node* b = arr.append(3);
-    arr.append(4);
-    arr.append(5);
-    arr.print();
-    std::cout << arr.begin->data << " " << arr.begin->next->data << std::endl;
-    arr.insert(2);
-    std::cout << arr.begin->data << " " << arr.begin->next->data << std::endl;
-    std::cout << arr.len() << "\n";
-    std::cout << arr.get_node(3)->data;
-    return 0;
-}
- 
+
