b6c5e308e684ddec74f7ee830f0df8217d0b6649
Popacopa <paushakow@gmail.com>
2024-12-03 00:46:08 +0300

    added init function

--- graph/main.cpp
+++ graph/main.cpp
@@ -1,36 +1,42 @@
 #include "graph_type.hpp"
-#define MARK std::cout << ">";
+
+
+Graph* graph = nullptr;
+bool** mat = nullptr;
 
 void input(unsigned int& size, bool** mat) {
     for (int y = 0; y < size; y++) {  
         std::cout << y + 1 << " строка:\n";
         for (int x = 0; x < size; x++) {  
-            MARK;std::cin >> mat[y][x];
+            std::cout << ">"; std::cin >> mat[y][x];
         }
     }
 }
 
-void buid_graph(unsigned int& size, bool** mat, Graph& graph) {
+void buid_graph(unsigned int& size, bool** mat, Graph* graph) {
     for (int y = 0; y < size; y++) {  
         for (int x = 0; x < size; x++) {  
             if (mat[y][x]) {
-                graph.addEdge(y, x);
+                graph->addEdge(y, x);
             }
         }
     }
-    graph.print();
+    graph->print();
 }
+void init(unsigned int& size) {
+    graph = new Graph(size);
+    mat = new bool*[size];
+    for (int i = 0; i < size; i++) {mat[i] = new bool[size];}
+}
+
 int main(int argc, char const *argv[])
 {
     bool check = 0;
     unsigned int size;
     std::cout << "0: запись в терминале || 1: запись из файла: "; std::cin >> check;
     if (!check) {
-        std::cout << "кол-во вершин графа: ";
-        std::cin >> size;
-        Graph graph(size);
-        bool** mat = new bool*[size];
-        for (int i = 0; i < size; i++) {mat[i] = new bool[size];}
+        std::cout << "кол-во вершин графа: "; std::cin >> size;
+        init(size);
         std::cout << "матрица смежности: " << "\n";
         input(size, mat);
         std::cout << "\n";
